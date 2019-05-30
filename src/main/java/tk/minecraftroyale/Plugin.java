82b5fbb1a4a1ee8f729075f691126db9690bac24
Will Leverone <barrelwolf38087@gmail.com>
2019-05-30 17:17:38 -0400

    Fixed things and it builds now

--- src/main/java/tk/minecraftroyale/Plugin.java
+++ src/main/java/tk/minecraftroyale/Plugin.java
@@ -3,5 +3,14 @@ package tk.minecraftroyale;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
-    
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabled");
+    }
 }
