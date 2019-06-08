bff77c7966f3998b5aa381ee9b168a55c82bffd0
maxh65000 <booah8@gmail.com>
2019-06-08 06:04:51 -0400

    Fix

--- src/main/java/tk/minecraftroyale/WorldStuff/RoyaleWorlds.java
+++ src/main/java/tk/minecraftroyale/WorldStuff/RoyaleWorlds.java
@@ -98,6 +98,7 @@ public class RoyaleWorlds {
 
         MinecraftRoyale.currentRound = new Round((MinecraftRoyale) plugin, new Time(0, 0, 0l, plugin.getConfig().getLong("timeConfig.roundDuration"), 0l), newWorld, () -> setUpWorldBorder(newWorld, true));
         MinecraftRoyale.currentRound.teleportAllToRoundWorld();
+        Bukkit.broadcastMessage("STARTING NEW ROUND # " + newWorld.getName().substring(5));
     }
 
     public void generateWorld(int roundNum) throws IllegalArgumentException, IOException, ConfigException {
